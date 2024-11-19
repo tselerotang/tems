ec5918b2e6532773bc4dbafdcc958c97eaf79b68
Rotang Daniels <rotangdaniels@gmail.com>
2024-11-19 19:38:46 +0200

    Updated next.config.js for image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['images.unsplash.com', 'source.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
@@ -8,8 +9,15 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
